f1301473aae42133099b172e300c1647878dffc5
Frederik Petersen <frederik@the-imperfection.de>
2019-08-22 23:02:10 +0200

    Tank has a gun and can be controlled with arrow keys

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,31 @@ const { canvas, context } = kontra.init();
 
 const tank = new Tank(50, 600);
 
+kontra.initKeys();
+
 const loop = kontra.GameLoop({  // create the main game loop
     render: function render() { // render the game state
         tank.render();
     },
-    update: function update() { // update the game state
+    update: function update(dt: number) { // update the game state
         tank.update();
 
         // wrap the sprites position when it reaches
         // the edge of the screen
         if (tank.x > canvas.width) {
             tank.x = -tank.width;
+        }
+
+        if (kontra.keyPressed("up")) {
+            tank.liftGun(dt);
+        } else if (kontra.keyPressed("down")) {
+            tank.lowerGun(dt);
+        }
+
+        if (kontra.keyPressed("right")) {
+            tank.goRight(dt);
+        } else if (kontra.keyPressed("left")) {
+            tank.goLeft(dt);
         }
     },
 });
--- src/sprites/tank.ts
+++ src/sprites/tank.ts
@@ -7,10 +7,13 @@ export class Tank extends kontra.Sprite.class {
     private radius = 10;
     private height = 20;
 
+    private gunRotation = 0;
+
+    private faceLeft = false;
+
     constructor(x: number, y: number) {
         super({
             color: "grey",
-            dx: 2,
             x: 100,
             y: 600,
         });
@@ -19,22 +22,55 @@ export class Tank extends kontra.Sprite.class {
     public render() {
         const context = this.context;
 
+        context.save();
+        context.translate(this.x + this.width / 2, this.y);
+        if (this.faceLeft) {
+            context.scale(-1, 1);
+        }
         context.beginPath();
         context.fillStyle = "silver";
-        context.arc(this.x + (this.width / 2), this.y, this.radius, Math.PI, 0);
+        context.arc(0, 0, this.radius, Math.PI, 0);
         context.fill();
         context.beginPath();
         context.fillStyle = "grey";
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.fillRect(- this.width / 2, 0, this.width, this.height);
         context.beginPath();
         context.fillStyle = "black";
-        context.ellipse(this.x + (this.width / 2), this.y + this.height / 1.5,
+        context.ellipse(0, this.height / 1.5,
             this.width / 1.5, this.height / 2, 0, 0, Math.PI * 2);
         context.fill();
         context.beginPath();
         context.fillStyle = "grey";
-        context.ellipse(this.x + (this.width / 2), this.y + this.height / 1.5,
+        context.ellipse(0, this.height / 1.5,
             this.width / 2, this.height / 2.8, 0, 0, Math.PI * 2);
         context.fill();
+        context.translate(0, -4);
+        context.rotate(this.gunRotation);
+        context.fillStyle = "silver";
+        context.fillRect(0, -2, 30, 4);
+        context.restore();
+
+    }
+
+    public liftGun(dt: number) {
+        if (this.gunRotation >  ((- Math.PI / 2) + (Math.PI / 6))) {
+            this.gunRotation = this.gunRotation - Math.PI * (dt / 5);
+        }
+    }
+
+    public lowerGun(dt: number) {
+        if (this.gunRotation < (0 + Math.PI / 16)) {
+            this.gunRotation = this.gunRotation + Math.PI * (dt / 5);
+        }
+    }
+
+    public goLeft(dt: number) {
+        super.x = this.x - dt * 50;
+        this.faceLeft = true;
+    }
+
+    public goRight(dt: number) {
+        super.x = this.x + dt * 50;
+        this.faceLeft = false;
     }
 }
